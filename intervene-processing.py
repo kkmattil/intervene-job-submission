a9946115f75561567ce09ba54681437e2e93253a
kkmattil <kimmo.mattila@csc.fi>
2022-10-17 15:56:27 +0300

    Update intervene-processing.py
    First version

--- intervene-processing.py
+++ intervene-processing.py
@@ -9,8 +9,6 @@ Created on Tue Oct  4 17:40:27 2022
 
 import json
 import datetime
-import time
-import sys
 import re
 from keystoneauth1 import session
 from keystoneauth1.identity import v3
@@ -19,6 +17,20 @@ import swiftclient
 from swiftclient.multithreading import OutputManager
 from swiftclient.service import SwiftError, SwiftService, SwiftUploadObject
 import getpass
+
+
+def get_task_json(task):
+    object_contents=conn.get_object(bucket, biobank+"/requests/"+task, headers=None)[1]
+    oc_string=str(object_contents)
+    task_json_link=oc_string.split(sep="'")[1]
+    task_json_link.strip
+    #print(task_json_link)
+
+    task_json=conn.get_object(bucket, task_json_link)[1]
+    task_description=json.loads(task_json)
+    return task_description
+    
+    #task_desc_txt=json.dumps(task_description, indent=2)
 
 
 def upload_large_file(bucket_name, object_name):
@@ -41,6 +53,7 @@ def upload_large_file(bucket_name, object_name):
 def download_large_file(bucket_name, object_name, out_file):
     # limit upload threads to 4
                 opts = {'object_uu_threads': 4, 'os_auth_url' : _authurl, 'os_username' : _user, 'os_password' : _key, 'os_project_name' : _project, 'os_project_domain_name' : 'Default', 'out_file' : out_file } 
+                #print(opts)
                 with SwiftService(options=opts) as swift:
                     for r in swift.download(bucket_name, [ object_name ]):
                         if r['success']:
@@ -78,8 +91,57 @@ def selectFromDict(options, name):
   return selected
 
 
-# Main program starts here
+def update_biobank_tasks(conn,biobank):
+    
+   #list all objects in the bucket
+   all_objs=conn.get_container(bucket)[1] 
+   my_tasks=[]
+   print("Collecting task infomration.\n")
+   for j in all_objs:
+      if re.search(biobank+"/requests/", j["name"] ):
+         #if re.search("/status/", j["name"] ):
+         task_row=(j["name"])
+         task=task_row.split("/")[-1]
+         my_tasks.append(task)  
+         # task_selection_dict[task] = task
 
+
+   ready_tasks=[]
+   tasks_processing=[]
+   waiting_tasks=[]
+   for task in my_tasks:
+      task_stat_num=0
+      for jj in all_objs:
+        if re.search(task+"/"+biobank+"/status/ready", jj["name"]):
+           task_stat_num=task_stat_num+4
+          #print(jj["name"]+" ready"+task_stat_num)
+        if re.search(task+"/"+biobank+"/status/processing", jj["name"]):
+            task_stat_num=task_stat_num+2
+          #print(jj["name"]+" proc")
+        if re.search(task+"/"+biobank+"/status/submitted", jj["name"]):
+          task_stat_num=task_stat_num+1
+          #print(jj["name"]+" submitted")
+      if task_stat_num==7 or task_stat_num==5:
+          ready_tasks.append(task)
+          task_selection_dict[task+" (ready)"] = task
+      if task_stat_num==3:
+          tasks_processing.append(task)
+          task_selection_dict[task+" (active)"] = task
+          task_upload_dict[task] = task
+      if task_stat_num==1:
+           waiting_tasks.append(task)
+           task_selection_dict[task+" (waiting)"] = task
+           task_download_dict[task]=task
+           
+   return(my_tasks, ready_tasks, tasks_processing, waiting_tasks, task_selection_dict, task_upload_dict, task_download_dict)
+#print(my_tasks)
+
+#target_task=selectFromDict(task_selection_dict, "task to be processed")
+
+
+#
+# Main program starts here
+#
 
 
 #parameters for allas connection
@@ -88,7 +150,6 @@ _authurl = 'https://pouta.csc.fi:5001/v3'
 _auth_version = '3'
 _user = input('CSC username: \n')
 _key = getpass.getpass('CSC password: \n')
-
 storage="allas"
 
 _os_options = {
@@ -116,128 +177,140 @@ available_biobanks['HUS'] = 'HUS'
 
 #select right biobank
 biobank=selectFromDict(available_biobanks, "Select your Biobank.")
- 
+
 ##List my tasks
 my_tasks=[]
 task_selection_dict = {}
-print("Tasks submitted to be processed in  "+biobank)
+task_download_dict= {}
+task_upload_dict={}
 
-#list all objects in the bucket
-all_objs=conn.get_container(bucket)[1] 
-
-#print("look for biobank specific requests")
-for j in all_objs:
-      if re.search(biobank+"/requests/", j["name"] ):
-         #if re.search("/status/", j["name"] ):
-         task_row=(j["name"])
-         task=task_row.split("/")[-1]
-         my_tasks.append(task)  
-         task_selection_dict[task] = task
-
-
-#1print ("check status of tasks")
-status_levels=["submitted", "processing", "ready" ]
-for task in my_tasks:
-    task_stat_num=-1
-    for jj in all_objs:
-        if re.search(task+"/"+biobank+"/status/ready", jj["name"]):
-          task_stat_num=task_stat_num+1
-          #print(jj["name"]+" ready"+task_stat_num)
-        if re.search(task+"/"+biobank+"/status/processing", jj["name"]):
-          task_stat_num=task_stat_num+1
-          #print(jj["name"]+" proc")
-        if re.search(task+"/"+biobank+"/status/submitted", jj["name"]):
-          task_stat_num=task_stat_num+1
-          #print(jj["name"]+" submitted")
-    print(task+ " "+status_levels[task_stat_num])
-        
-            
-
-#print(my_tasks)
-
-target_task=selectFromDict(task_selection_dict, "task to be processed")
+#print("Tasks submitted to be processed in  "+biobank)
+my_tasks, ready_tasks, tasks_processing, waiting_tasks, task_selection_dict, task_upload_dict, task_download_dict = update_biobank_tasks(conn,biobank)
 
 
 #select operation
+operation=""
 operations = {}
+operations["Show task list"] = "list"
 operations["Show the job request file"] = "info"
 operations["Download task for processing"] = "download"
 operations["Upload results"] = "upload"
+operations["Quit"] = "quit"
 
-operation=selectFromDict(operations, "operation")
-
-
-#read the description of selected job
-object_contents=conn.get_object(bucket, biobank+"/requests/"+target_task, headers=None)[1]
-oc_string=str(object_contents)
-task_json_link=oc_string.split(sep="'")[1]
-task_json_link.strip
-print(task_json_link)
-
-task_json=conn.get_object(bucket, task_json_link)[1]
-task_description=json.loads(task_json)
-
-
-if operation == "info":  
-   task_desc_txt=json.dumps(task_description, indent=2)
-   print(task_desc_txt)
-
-
-if operation == "download":
-   downloaddir=target_task
-   isExist = os.path.exists(downloaddir)
-   if not isExist:
-     # Create a new directory because it does not exist
-     os.makedirs(downloaddir)
+# keep running untill quit
+while operation != "quit":
+   operation=selectFromDict(operations, "operation")
    
-   
-   # Inputfiles to download
-   for input_file in task_description["inputs"]:
-       #print("Downloading "+bucket+input_file["url"]+" to "+target_task+input_file["path"])
-       download_large_file(bucket, input_file["object"], target_task+"/"+input_file["path"])
-   
-   for input_file in task_description["instructions"]:
-       download_large_file(bucket, input_file["object"], target_task+"/README_instructions")
-   
-   #    print(input_file["url"]+"->"+target_task+"/"+input_file["path"])
-   #   print("\n")
-   
-   
-   #create status object
-   workdir=(task_description["csc-user"]+"/"+target_task)
-   
-   status_object=("jobs/"+workdir+"/"+biobank+"/status/processing")
-   conn.put_object(bucket, status_object ,
-                  contents=(_user+": "+str(datetime.datetime.now())),
-                  content_type='text/plain')
-   
-   
-   
-   print("Job downloaded to local directory "+target_task)
-   print("Task specific instaructions are available in: "+target_task+"/README_instructions")
-   
-   
-if operation == "upload":
-   print("Cheking result files.")     
-   for output_file in task_description["outputs"]:
-       if os.path.isfile(output_file["path"]):
-             print("Result file "+output_file["path"]+" found.")                   
-       else:   
-          print("ERROR: Result file "+output_file["path"]+"not found!")
-          quit()  
+   #
+   # List all tasks
+   #
+   if operation=="list":
+       my_tasks, ready_tasks, tasks_processing, waiting_tasks, task_selection_dict, task_upload_dict, task_download_dict = update_biobank_tasks(conn,biobank)
+       
+       print("-----------------------------------------------------------")
+       print("Ready tasks:")
+       for task in ready_tasks:
+           task_name=get_task_json(task)["name"]
+           print(task+" "+task_name)
+       print("-----------------------------------------------------------")
+       print("Tasks that have already been downloaded for processing:")
+       for task in tasks_processing:
+           task_name=get_task_json(task)["name"]
+           print(task+" "+task_name)
+       print("-----------------------------------------------------------")    
+       print("Tasks that are waiting to be downloaded for processing:")
+       for task in waiting_tasks:
+           task_name=get_task_json(task)["name"]
+           print(task+" "+task_name+" Waiting.")
+       print("-----------------------------------------------------------")
+   #
+   #show info for one task
+   #
+   if operation=="info":
+      target_task=selectFromDict(task_selection_dict, "task to be processed")
+      task_description=get_task_json(task)
     
-   print("All result files found. startng upload")
-   for output_file in task_description["outputs"]:         
-       upload_large_file(output_file["url"], output_file["path"]) 
-       
-       
-   #create status object
-   workdir=(task_description["csc-user"]+"/"+target_task)
+      task_desc_txt=json.dumps(task_description, indent=2)
+      print(task_desc_txt)
+
+
+   #
+   # download a task
+   #
+   if operation == "download":
+      my_tasks, ready_tasks, tasks_processing, waiting_tasks, task_selection_dict, task_upload_dict, task_download_dict = update_biobank_tasks(conn,biobank)
+      target_task=selectFromDict(task_download_dict, "Task to be downloaded for processing.")
+      task_description=get_task_json(target_task)
+      downloaddir=target_task
+      isExist = os.path.exists(downloaddir)
+      print("Dir test")
+      if not isExist:
+         # Create a new directory because it does not exist
+         os.makedirs(downloaddir)
    
-   status_object=("jobs/"+workdir+"/"+biobank+"/status/ready")
-   conn.put_object(bucket, status_object ,
+      print("Get inputs")
+      # Inputfiles to download
+      for input_file in task_description["inputs"]:
+          print(" ")
+          print("Downloading "+input_file["url"]+" to "+target_task+"/"+input_file["path"])
+          print(" ")
+          download_large_file(input_file["bucket"], input_file["object"], target_task+"/"+input_file["path"])
+ 
+      print("get instuctions")
+      # instuctions file    
+      for input_file in task_description["instructions"]:
+          download_large_file(input_file["bucket"], input_file["object"], target_task+"/README_instructions")
+   
+          #    print(input_file["url"]+"->"+target_task+"/"+input_file["path"])
+          #   print("\n")
+      print("write json")
+     #task_json
+      with open(target_task+"/task.json", "w") as outfile:
+          outfile.write(json.dumps(task_description, indent=2))
+          outfile.close 
+   
+      #create status object
+      workdir=(task_description["csc-user"]+"/"+target_task)
+   
+      status_object=("jobs/"+workdir+"/"+biobank+"/status/processing")
+      conn.put_object(bucket, status_object ,
                   contents=(_user+": "+str(datetime.datetime.now())),
                   content_type='text/plain')
    
    
-   
+      my_tasks, ready_tasks, tasks_processing, waiting_tasks, task_selection_dict, task_upload_dict, task_download_dict = update_biobank_tasks(conn,biobank)
+      print("Job downloaded to local directory "+target_task)
+      print("Task specific instaructions are available in: "+target_task+"/README_instructions")
+        
+   #
+   # uoload a ready task
+   #
+   if operation == "upload":
+       
+       target_task=input('Task to upload: \n')
+       task_description=get_task_json(task)
+       #target_task=selectFromDict(task_download_dict, "Task to be uploaded for processing.")
+       
+       print("Cheking result files.")     
+       for output_file in task_description["outputs"]:
+           if os.path.isfile(output_file["path"]):
+              print("Result file "+output_file["path"]+" found.")                   
+           else:   
+              print("ERROR: Result file "+output_file["path"]+"not found!")
+              quit()  
+    
+       print("All result files found. startng upload")
+       for output_file in task_description["outputs"]:       
+          print("uploadig: "+output_file["url"]+"->"+output_file["path"])
+          upload_large_file(bucket+"/jobs/"+task_description["csc-user"]+"/"+target_task+"/"+biobank+"/results", output_file["path"])
+          #upload_large_file(output_file["url"], output_file["path"]) 
+       
+       
+       #create status object
+       workdir=(task_description["csc-user"]+"/"+target_task)
+
+       status_object=("jobs/"+workdir+"/"+biobank+"/status/ready")
+       conn.put_object(bucket, status_object ,
+                  contents=(_user+": "+str(datetime.datetime.now())),
+                  content_type='text/plain')
+       my_tasks, ready_tasks, tasks_processing, waiting_tasks, task_selection_dict, task_upload_dict, task_download_dict = update_biobank_tasks(conn,biobank)
